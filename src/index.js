087d715d5ab50e66a3c00fb0ab5ce8de38d7f4ee
Joseph Daudi <josedaudi@outlook.com>
2020-03-24 13:18:18 +0300

    Added custom css styles to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/kanban.css'
+import 'react-quill/dist/quill.snow.css'
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
